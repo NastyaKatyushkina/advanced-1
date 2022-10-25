65455741bcd634364f5c78237adf394a644ad66e
NastyaKatyushkina <nastyusha_kat@mail.ru>
2022-10-25 22:48:10 +0500

    advanced-1

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,12 +1,9 @@
 import sortProps from '../app';
-
 test('should sort object properties correctly when there are sorting keys', () => {
   const inputObject = {
     name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
   };
   const inputArray = ['name', 'level'];
-
-
   const expected = [
     { key: 'name', value: 'мечник' },
     { key: 'level', value: 2 },
@@ -14,18 +11,13 @@ test('should sort object properties correctly when there are sorting keys', () =
     { key: 'defence', value: 40 },
     { key: 'health', value: 10 },
   ];
-
-
   const recieved = sortProps(inputObject, inputArray);
   expect(recieved).toEqual(expected);
 });
-
 test('should sort object properties correctly without sorting keys', () => {
   const inputObject = {
     name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
   };
-
-
   const expected = [
     { key: 'attack', value: 80 },
     { key: 'defence', value: 40 },
@@ -33,7 +25,6 @@ test('should sort object properties correctly without sorting keys', () => {
     { key: 'level', value: 2 },
     { key: 'name', value: 'мечник' },
   ];
-
   const recieved = sortProps(inputObject);
   expect(recieved).toEqual(expected);
 });
